7c9036ceefc64bd0d6a8d2bfb27184e187b91f87
Jingxin Wang <wjxalexander@gmail.com>
2023-03-06 22:34:58 +0800

    refactor

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -3,34 +3,87 @@ package com.github.hcsp;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
+    private static boolean isTargetLink(String link) {
+        return link.startsWith("https://news.sina.cn");
+    }
+
+    private static Document getAndParseHtml(String link) {
+        if (link.startsWith("//")) {
+            link = "https://" + link;
+        }
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
-            ClassicHttpRequest httpGet = ClassicRequestBuilder.get("https://www.sina.cn/")
+            ClassicHttpRequest httpGet = ClassicRequestBuilder.get(link)
                     .build();
-            // The underlying HTTP connection is still held by the response object
-            // to allow the response content to be streamed directly from the network socket.
-            // In order to ensure correct deallocation of system resources
-            // the user MUST call CloseableHttpResponse#close() from a finally clause.
-            // Please note that if response content is not fully consumed the underlying
-            // connection cannot be safely re-used and will be shut down and discarded
-            // by the connection manager.
-            httpclient.execute(httpGet, response -> {
-                System.out.println(response.getCode() + " " + response.getReasonPhrase());
-                final HttpEntity entity1 = response.getEntity();
-                // do something useful with the response body
-                // and ensure it is fully consumed
-                System.out.println(EntityUtils.toString(entity1));
-                return null;
-            });
+            httpGet.addHeader("user-agent:", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36");
+            return httpclient.execute(httpGet, Main::handleResponse);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void main(String[] args) {
+        List<String> linkPool = new ArrayList<>();
+        linkPool.add("https://news.sina.cn/");
+        Set<String> visitedLinkPool = new HashSet<>();
+
+        while (true) {
+            if (linkPool.isEmpty()) {
+                break;
+            }
+
+            String link = linkPool.remove(linkPool.size() - 1);
+            if (visitedLinkPool.contains(link)) {
+                continue;
+            }
+
+            if (isTargetLink(link)) {
+                System.out.println(link);
+                Document doc = getAndParseHtml(link);
+                doc.select("a").stream().map(linkTag -> linkTag.attr("a")).forEach(linkPool::add);
+                storeNewsIntoDb(doc);
+                visitedLinkPool.add(link);
+            }
+        }
+    }
+
+    private static void storeNewsIntoDb(Document doc) {
+        Elements articleTags = doc.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element articleTag : articleTags) {
+                Elements headers = articleTag.select("h1");
+                if (!headers.isEmpty()) {
+                    String title = headers.get(0).text();
+                    System.out.println(title);
+                }
+            }
+        }
+    }
+
+    private static Document handleResponse(ClassicHttpResponse response) {
+        final HttpEntity entity1 = response.getEntity();
+        String htmlContent;
+        try {
+            htmlContent = EntityUtils.toString(entity1);
+        } catch (IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return Jsoup.parse(htmlContent);
     }
 }
